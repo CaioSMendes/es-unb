eef143b1c5e9f34f50175e15d8fcc2f898095cf2
caeira <caioitallo.souzamendes@gmail.com>
2019-12-05 14:20:45 -0200

    agr ta certo

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,4 @@
 class ActivitiesController < ApplicationController
-  before_action :authenticate_admin!
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
   # GET /activities
